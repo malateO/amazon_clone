29300d0ad9278e78807597091e4762295949bbde
Oliver Malate <oliver95malate@gmail.com>
2024-06-21 03:51:15 +0800

    make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -84,6 +84,13 @@ addToCartBtn.forEach((button) => {
         quantity: 1,
       });
     }
-    console.log(cart);
+
+    let cartQuantity = 0;
+
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
   });
 });
