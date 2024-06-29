0a4030c01a1d3ecd9d026c0e08a61478930f9842
Oliver Malate <oliver95malate@gmail.com>
2024-06-30 01:43:05 +0800

    learn more details about classes and 'this'

--- data/products.js
+++ data/products.js
@@ -56,6 +56,34 @@ class Clothing extends Product {
     return `<a href="${this.sizeChartLink}" target="_blank">Size chart</a>`;
   }
 }
+/*
+const date = new Date();
+console.log(date);
+console.log(date.toLocaleTimeString());
+*/
+/*
+console.log(this);
+
+const object2 = {
+  a: 2,
+  b: this.a,
+};
+*/
+/*
+function logThis() {
+  console.log(this);
+}
+logThis();
+logThis.call("hello");
+
+this;
+const object3 = {
+  method: () => {
+    console.log(this);
+  },
+};
+object3.method();
+*/
 
 export const products = [
   {
