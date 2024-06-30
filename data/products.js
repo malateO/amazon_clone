7255f45d198145b18d73a340208c871ecb645500
Oliver Malate <oliver95malate@gmail.com>
2024-07-01 03:14:38 +0800

    learn error handling

--- data/products.js
+++ data/products.js
@@ -101,6 +101,9 @@ export function loadProductsFetch() {
       });
 
       console.log("load products");
+    })
+    .catch((error) => {
+      console.log("Unexpected error. Please try again later");
     });
   return promise;
 }
@@ -124,6 +127,10 @@ export function loadProducts(fun) {
     console.log("load products");
 
     fun();
+  });
+
+  xhr.addEventListener("error", (error) => {
+    console.log("Unexpected error. Please try again later");
   });
 
   xhr.open("GET", "https://supersimplebackend.dev/products");
